64184edd60be7f907fbc1c033c798e4596f37982
Vish0111 <it21315282@my.sliit.lk>
2024-03-12 01:23:35 +0530

    did some changes

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(cors());
 
 app.get('/', (req, res) => {
-    res.send('Server is running successfully');
+    res.send('Hello World!');
   });
 
 // Define the port for the server to listen on
