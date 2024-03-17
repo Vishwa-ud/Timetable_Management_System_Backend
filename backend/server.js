75dbcada22412430d929eebc4f3894fe9ee6e745
Vish0111 <it21315282@my.sliit.lk>
2024-03-18 01:57:24 +0530

    course route path added

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ const connection = require('./db');
 // Import routes
 const UserRoutes = require('./routes/users');
 const AuthRoutes = require('./routes/auth');
+const coursesRouter = require('./routes/courses');
 
 // Connect to database
 connection();
@@ -18,6 +19,8 @@ app.use(cors());
 //Routes
 app.use('/api/users', UserRoutes);
 app.use('/api/auth', AuthRoutes);
+app.use('/api/courses', coursesRouter);
+
 
 
 
