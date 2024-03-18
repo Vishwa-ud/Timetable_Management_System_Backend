57df1839735ad2de8b76869168aecebb95a64bbc
Vish0111 <it21315282@my.sliit.lk>
2024-03-18 23:29:21 +0530

    User Roles Acces Fixed

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,20 +1,27 @@
 const jwt = require('jsonwebtoken');
 const secretKey = process.env.JWTPRIVATEKEY;
 
-// Middleware for authenticating user based on JWT token
 function authenticateUser(req, res, next) {
     const token = req.header('Authorization');
+
+    // Check if token exists
     if (!token) return res.status(401).send({ message: 'Access denied. No token provided.' });
 
     try {
-        const decoded = jwt.verify(token, secretKey);
+        // Remove "Bearer " prefix if it exists
+        const tokenWithoutBearer = token.replace(/^Bearer\s/, '');
+
+        // Verify token without "Bearer" prefix
+        const decoded = jwt.verify(tokenWithoutBearer, process.env.JWTPRIVATEKEY);
+
+        // Attach decoded user information to the request object
         req.user = decoded;
         next();
     } catch (error) {
+        // If token verification fails, send an error response
         res.status(400).send({ message: 'Invalid token.' });
     }
 }
-
 // Middleware for authorizing user based on role
 function authorizeRole(roles) {
     return (req, res, next) => {
--- backend/models/user.js
+++ backend/models/user.js
@@ -15,7 +15,10 @@ const userSchema = new mongoose.Schema({
 });
 
 userSchema.methods.generateAuthToken = function() {
-    const token = jwt.sign({ _id: this._id }, process.env.JWTPRIVATEKEY, { expiresIn: '7d' });
+    const token = jwt.sign({ 
+        _id: this._id,
+        role: this.role // Include the user's role in the token payload
+    }, process.env.JWTPRIVATEKEY, { expiresIn: '7d' });
     return token;
 };
 
