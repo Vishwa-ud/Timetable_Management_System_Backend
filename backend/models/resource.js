a9dc1863a082aeeeb576bafac326af2a6c68a2e2
Vish0111 <it21315282@my.sliit.lk>
2024-03-19 15:25:53 +0530

    resourse model added

---
+++ backend/models/resource.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const resourceSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    quantity: { type: Number, required: true }
+});
+
+const Resource = mongoose.model('Resource', resourceSchema);
+
+module.exports = Resource;
