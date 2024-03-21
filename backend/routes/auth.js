312b58649b864b071fc01a4b21a3dcbea6a89626
Vish0111 <it21315282@my.sliit.lk>
2024-03-21 23:25:42 +0530

    logers added to auth and users

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const { User } = require('../models/user');
 
+const { logger } = require('./logger');
+
 //validate function
 const joi = require('joi');
 
@@ -9,19 +11,27 @@ const bcrypt = require('bcrypt');
 router.post("/", async (req, res) => {
     try{
         const {error} = validate(req.body);
-        if(error)
+        if(error){
+            logger.error('Validation error:', error);
             return res.status(400).send({message: error.details[0].message});
+        }    
         const user = await User.findOne({email: req.body.email});
-        if(!user)
+        if(!user){
+            logger.info('Invalid email or password:', req.body.email);
             return res.status(401).send({message: "Invalid email or password!"});
+        }
         const validPassword = await  bcrypt.compare(
             req.body.password, user.password
             );  
-        if(!validPassword)
+        if(!validPassword){
+            logger.info('Invalid email or password:', req.body.email);
             return res.status(401).send({message: "Invalid email or password!"});
+        }
         const token = user.generateAuthToken();
+        logger.info('Login successful for user:', req.body.email);
         res.status(200).send({ data: { token, role: user.role }, message: "Login successful!" });
     }catch(error){
+        logger.error('Internal server error:', error);
         res.status(500).send({message: "Internal server error!"});
 
     }
